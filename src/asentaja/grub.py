0392e1afdb21cd3ee621aa8ef74148992957159f
Windore <windore.wi@protonmail.com>
2022-12-26 01:00:57 +0200

    Päivitä Grub oletusasetuksia

--- src/asentaja/grub.py
+++ src/asentaja/grub.py
@@ -2,15 +2,27 @@ import asentaja
 
 class Grub:
     kernel_parametrit = []
-    oletus = 0
-    aikakatkaisu = 5
     aktivoi_os_prober = True
-    extra_asetukset = ""
+    oletusasetukset = """# Tämä tiedosto on Asentajan automaattisesti luoma.
+GRUB_CMDLINE_LINUX=""
+
+# Grub muistaa automaattisesti viimeksi valitun tiedoston
+GRUB_DEFAULT=saved
+GRUB_SAVEDEFAULT=true
+
+# Oletusasetuksia yhdestä automaattisesti luodusta tiedostosta
+GRUB_TIMEOUT=5
+GRUB_DISTRIBUTOR=Arch
+GRUB_PRELOAD_MODULES="part_gpt part_msdos"
+GRUB_TIMEOUT_STYLE=menu
+GRUB_TERMINAL_INPUT=console
+GRUB_GFXMODE=auto
+GRUB_GFXPAYLOAD_LINUX=keep
+GRUB_DISABLE_RECOVERY=true"""
+    lisäasetukset = ""
 
     def asenna(self):
-        grub_asetukset = f"""# Tämä tiedosto on Asentajan automaattisesti luoma.
-GRUB_DEFAULT={self.oletus}
-GRUB_TIMEOUT={self.aikakatkaisu}
+        grub_asetukset = f"""{self.oletusasetukset}
 GRUB_CMDLINE_LINUX_DEFAULT="{" ".join(self.kernel_parametrit)}"
 """
         if self.aktivoi_os_prober:
@@ -19,7 +31,7 @@ GRUB_CMDLINE_LINUX_DEFAULT="{" ".join(self.kernel_parametrit)}"
         else:
             grub_asetukset += "GRUB_DISABLE_OS_PROBER=true\n"
 
-        grub_asetukset += self.extra_asetukset
+        grub_asetukset += self.lisäasetukset
         asentaja.tiedostot["/etc/default/grub"] = asentaja.Tiedosto(sisältö=grub_asetukset)
 
 
