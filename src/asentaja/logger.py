27ee54027ace85b87d3964c13706a3e072716916
Windore <windore.me@proton.me>
2023-01-03 18:26:37 +0200

    Paranna stdout ulkoasua

--- src/asentaja/logger.py
+++ src/asentaja/logger.py
@@ -3,6 +3,7 @@ _keltainen_prefix = "\033[93m"
 _sininen_prefix = "\033[96m" 
 _harmaa_prefix = "\033[2m"
 _yleinen_suffix = "\033[m"
+_asentaja_tag = "\033[1;35m[Asentaja]\033[m"
 
 
 informatiivinen = True
@@ -10,15 +11,15 @@ informatiivinen = True
 
 def info(viesti):
     if informatiivinen:
-        print(f"{_sininen_prefix}INFO{_yleinen_suffix}:     {viesti}")
+        print(f"{_asentaja_tag} {_sininen_prefix}INFO{_yleinen_suffix}:     {viesti}")
 
 
 def varoitus(viesti):
-    print(f"{_keltainen_prefix}VAROITUS{_yleinen_suffix}: {viesti}")
+    print(f"{_asentaja_tag} {_keltainen_prefix}VAROITUS{_yleinen_suffix}: {viesti}")
 
 
 def virhe(viesti):
-    print(f"{_punainen_prefix}VIRHE{_yleinen_suffix}:    {viesti}")
+    print(f"{_asentaja_tag} {_punainen_prefix}VIRHE{_yleinen_suffix}:    {viesti}")
 
 
 def virhetiedot(virhe):
