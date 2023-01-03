2b91a666442fbda701f43ad2a5e6d23e94f01748
Windore <windore.me@proton.me>
2023-01-03 17:46:31 +0200

    Korjaa tallennettujen listojen luku
    Tallennetut listat muodostetaan nyt pelkästään rivinvaihtojen perusteella

--- src/asentaja/tallennus.py
+++ src/asentaja/tallennus.py
@@ -13,7 +13,7 @@ def lue_lista(nimi, oletus=[], salli_virheet=True):
     tiedosto = os.path.join(asentaja.asetukset.tiedostojärjestelmän_alku, _sijainti, nimi)
     try:
         with open(tiedosto, "rt") as t:
-            return t.read().split()
+            return t.read().split("\n")
     except OSError as e:
         if salli_virheet:
             log.varoitus(f"Asentajan oman tiedoston '{tiedosto}' luku epäonnistui.")
